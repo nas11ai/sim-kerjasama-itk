42c7816be20b8986a7c6fec60365e5df4dc6c62c
nas11ai <nas11ai@uniba-bpn.ac.id>
2025-05-15 13:14:12 +0800

    feat: init inertia vue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
