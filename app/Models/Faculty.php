4a648a4c771748cfed714fec2dfe1991b1f88e28
nas11ai <nas11ai@uniba-bpn.ac.id>
2025-06-05 08:59:19 +0800

    feat: init study program

--- app/Models/Faculty.php
+++ app/Models/Faculty.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Faculty extends Model
 {
     protected $fillable = ['name'];
+
+    public function studyPrograms()
+    {
+        return $this->hasMany(StudyProgram::class);
+    }
 }
---
+++ app/Models/StudyProgram.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StudyProgram extends Model
+{
+    protected $fillable = [
+        'faculty_id',
+        'name',
+    ];
+
+    public function faculty()
+    {
+        return $this->belongsTo(Faculty::class);
+    }
+}
