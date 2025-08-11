01953a6e8b41d7575575e45f6dd19b6f2bf41333
nas11ai <74126181+nas11ai@users.noreply.github.com>
2025-08-11 08:31:04 +0800

    feat: update submission dates table and model

--- app/Models/SubmissionDate.php
+++ app/Models/SubmissionDate.php
@@ -7,13 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class SubmissionDate extends Model
 {
     protected $fillable = [
-        'label',
-        'date',
+        'submission_date_label_id',
+        'datetime',
         'submission_period_id',
     ];
 
     public function submissionPeriod()
     {
         return $this->belongsTo(SubmissionPeriod::class);
+    }
+
+    public function submissionDateLabel()
+    {
+        return $this->belongsTo(SubmissionDateLabel::class);
     }
 }
---
+++ database/migrations/2025_08_11_002015_change_column_label_to_label_id_in_submission_dates.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('submission_dates', function (Blueprint $table) {
+            $table->dropColumn('label');
+            $table->dropColumn('date');
+        });
+
+        Schema::table('submission_dates', function (Blueprint $table) {
+            $table->foreignId('submission_date_label_id')
+                ->after('id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->dateTime('datetime')
+                ->after('submission_date_label_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('submission_dates', function (Blueprint $table) {
+            $table->dropForeign(['submission_date_label_id']);
+            $table->dropColumn('submission_date_label_id');
+            $table->dropColumn('datetime');
+            $table->string('label')->after('id');
+            $table->date('date')->after('label');
+        });
+    }
+};
