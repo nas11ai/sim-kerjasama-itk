c254986b1fef2f20c2ef1e2f30d48ec7b9407b27
nas11ai <kairupannasai@gmail.com>
2025-08-12 12:44:06 +0800

    feat: init utility function for submission date

--- app/Models/SubmissionDate.php
+++ app/Models/SubmissionDate.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class SubmissionDate extends Model
@@ -12,6 +13,10 @@ class SubmissionDate extends Model
         'submission_period_id',
     ];
 
+    protected $casts = [
+        'date' => 'datetime'
+    ];
+
     public function submissionPeriod()
     {
         return $this->belongsTo(SubmissionPeriod::class);
@@ -20,5 +25,25 @@ class SubmissionDate extends Model
     public function submissionDateLabel()
     {
         return $this->belongsTo(SubmissionDateLabel::class);
+    }
+
+    public function isPassed(): bool
+    {
+        return Carbon::now()->gt($this->date);
+    }
+
+    public function isToday(): bool
+    {
+        return Carbon::now()->isSameDay($this->date);
+    }
+
+    public function isUpcoming(): bool
+    {
+        return Carbon::now()->lt($this->date);
+    }
+
+    public function getDaysFromNow(): int
+    {
+        return Carbon::now()->diffInDays($this->date, false);
     }
 }
