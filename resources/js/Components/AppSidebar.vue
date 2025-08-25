ba4a4514893843dd204b40aee978e7c03d0cc693
Najer-jril <mnazrililham55@gmail.com>
2025-08-25 20:47:38 +0900

    feat: Implement role-based access control in sidebar for admin features

--- resources/js/Components/AppSidebar.vue
+++ resources/js/Components/AppSidebar.vue
@@ -31,6 +31,17 @@ import ResponsiveNavLink from "./ResponsiveNavLink.vue";
 const page = usePage();
 const user = page.props.auth.user;
 
+// console.log('User object:', user);
+
+const hasAdminAccess = computed(() => {
+    if (!user || !user.roles) {
+        return false;
+    }
+    return user.roles.includes('admin') || user.roles.includes('Super Admin');
+});
+
+// console.log('Has Admin Access:', hasAdminAccess.value);
+
 const isUserMenuOpen = ref(false);
 const toggleUserMenu = () => {
     isUserMenuOpen.value = !isUserMenuOpen.value;
@@ -138,7 +149,7 @@ const userManagementItems = [
 
         <SidebarContent>
             <!-- Main Menu -->
-            <SidebarGroup>
+            <SidebarGroup v-if="hasAdminAccess">
                 <SidebarGroupLabel>Main Menu</SidebarGroupLabel>
                 <SidebarGroupContent>
                     <SidebarMenu>
@@ -156,7 +167,7 @@ const userManagementItems = [
             </SidebarGroup>
 
             <!-- Form Management -->
-            <SidebarGroup>
+            <SidebarGroup v-if="hasAdminAccess">
                 <SidebarGroupLabel>Form Management</SidebarGroupLabel>
                 <SidebarGroupContent>
                     <SidebarMenu>
@@ -174,7 +185,7 @@ const userManagementItems = [
             </SidebarGroup>
 
             <!-- Academic Management -->
-            <SidebarGroup>
+            <SidebarGroup v-if="hasAdminAccess">
                 <SidebarGroupLabel>Academic Management</SidebarGroupLabel>
                 <SidebarGroupContent>
                     <SidebarMenu>
@@ -192,7 +203,7 @@ const userManagementItems = [
             </SidebarGroup>
 
             <!-- Submission Management -->
-            <SidebarGroup>
+            <SidebarGroup v-if="hasAdminAccess">
                 <SidebarGroupLabel>Submission Management</SidebarGroupLabel>
                 <SidebarGroupContent>
                     <SidebarMenu>
