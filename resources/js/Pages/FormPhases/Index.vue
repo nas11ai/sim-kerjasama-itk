6653cf1f60e46865dd56b7028fca6dd891238ad8
Muhammad Shadiq Al-Fatiy <mshadiqaf@gmail.com>
2025-11-09 11:31:03 +0800

    style: adding search icon in search bar for ui purpose & maximized width of search bar form phase index

--- resources/js/Pages/FormPhases/Index.vue
+++ resources/js/Pages/FormPhases/Index.vue
@@ -176,9 +176,11 @@ const toggleStatus = (formPhase: FormPhase) => {
                 </CardHeader>
                 <CardContent>
                     <div class="flex items-center gap-4">
-                        <div class="flex-1">
+                        <div class="flex-1 relative">
+                            <Search
+                                class="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
                             <Input v-model="searchQuery" placeholder="Search form phases by title or description..."
-                                class="max-w-md" />
+                                class="pl-10" />
                         </div>
                     </div>
                 </CardContent>
