45b98261d4c66d49115237c2550a258847e4c096
nas11ai <kairupannasai@gmail.com>
2026-05-29 12:00:34 +0800

    fix: vue linting config

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,40 @@ import js from '@eslint/js'
 import vue from 'eslint-plugin-vue'
 import ts from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
+import vueParser from 'vue-eslint-parser'
 import prettierConfig from 'eslint-config-prettier'
+import globals from 'globals'
 
 export default [
     js.configs.recommended,
     ...vue.configs['flat/recommended'],
     {
-        files: ['**/*.ts', '**/*.vue'],
+        files: ['**/*.ts'],
         languageOptions: {
             parser: tsParser,
-            parserOptions: { extraFileExtensions: ['.vue'] },
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+            },
+        },
+        plugins: { '@typescript-eslint': ts },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'error',
+            'no-console': 'warn',
+        },
+    },
+    {
+        files: ['**/*.vue'],
+        languageOptions: {
+            parser: vueParser,
+            parserOptions: {
+                parser: tsParser,
+                extraFileExtensions: ['.vue'],
+            },
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+            },
         },
         plugins: { '@typescript-eslint': ts },
         rules: {
@@ -19,9 +43,6 @@ export default [
             'vue/component-api-style': ['error', ['script-setup']],
             'vue/no-unused-vars': 'error',
             'no-console': 'warn',
-        },
-        env: {
-            node: true,
         },
     },
     prettierConfig,
