a681ea7ead39a4dff104550642d5669e1b498241
Muhammad Nasa'i Kairupan <74126181+nas11ai@users.noreply.github.com>
2026-05-29 19:21:53 +0800

    [Chore] Add GitHub Actions CI workflow (#111)
    * docs: ci workflow
    
    * chore: update minIO healthcheck
    
    * docs: update contributing guide
    
    * chore: update package lock
    
    * fix: eslint config error
    
    * fix: vue linting config
    
    * fix: ziggy error
    
    * fix: function call in welcome page
    
    * refactor: add missing import

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,46 @@ import js from '@eslint/js'
 import vue from 'eslint-plugin-vue'
 import ts from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
+import vueParser from 'vue-eslint-parser'
 import prettierConfig from 'eslint-config-prettier'
+import globals from 'globals'
 
-js.configs.recommended,
-...vue.configs['flat/recommended'],
 export default [
+    {
+        ignores: ['resources/js/ziggy.js'],
+    },
+
     js.configs.recommended,
     ...vue.configs['flat/recommended'],
+
     {
-        files: ['**/*.ts', '**/*.vue'],
+        files: ['**/*.ts'],
         languageOptions: {
             parser: tsParser,
-            parserOptions: { extraFileExtensions: ['.vue'] },
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+            },
+        },
+        plugins: { '@typescript-eslint': ts },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'error',
+            'no-console': 'warn',
+        },
+    },
+
+    {
+        files: ['**/*.vue'],
+        languageOptions: {
+            parser: vueParser,
+            parserOptions: {
+                parser: tsParser,
+                extraFileExtensions: ['.vue'],
+            },
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+            },
         },
         plugins: { '@typescript-eslint': ts },
         rules: {
@@ -22,9 +50,7 @@ export default [
             'vue/no-unused-vars': 'error',
             'no-console': 'warn',
         },
-        "env": {
-            "node": true
-        }
     },
 
+    prettierConfig,
 ]
