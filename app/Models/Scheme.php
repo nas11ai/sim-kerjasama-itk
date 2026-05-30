7c647e2815f678b07b09792a54c17519334852f6
Alfian Ramadani <alfianr792@gmail.com>
2026-05-30 16:43:20 +0800

    feat:  Scheme model with rules handling methods

---
+++ app/Models/Scheme.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Scheme extends Model
+{
+protected $casts = ['rules' => 'array'];
+
+public function getRule(string $key, mixed $default = null): mixed
+{
+    return data_get($this->rules, $key, $default);
+}
+
+public function minReviewerCount(): int
+{
+    return (int) $this->getRule('min_reviewer_count', 2);
+}
+
+public function maxReviewerWorkload(): int
+{
+    return (int) $this->getRule('max_reviewer_workload', 10);
+}
+}
