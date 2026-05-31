b6c3ec31259acfa75b2dc6e4422d0ee1182c43b4
Alfian Ramadani <57712162+AlfianRamadani@users.noreply.github.com>
2026-05-31 14:07:49 +0800

    Task/90 add soft deletes to from fields (#129)
    * feat: Add soft deletes to form_fields migration
    
    * feat: Implement soft deletes in FormField model
    
    * fix: Correct table names in migration down methods for announcements and user_profiles
    
    * feat: Update foreign key constraints for form_field_id in form_field_responses migration

--- app/Models/FormField.php
+++ app/Models/FormField.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 
 class FormField extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'form_id',
         'label',
