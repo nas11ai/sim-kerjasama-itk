db50a22080cea6e31a4b28bdbb849c080adb61f2
Alfian Ramadani <57712162+AlfianRamadani@users.noreply.github.com>
2026-07-08 11:18:06 +0800

    feat: add migration to transfer faculties to organizations (#200)
    * feat: add migration to transfer faculties to organizations
    
    * chore: format code pint fix

---
+++ database/migrations/2026_07_07_135541_migrate_faculties_to_organizations.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::transaction(function () {
+            $rootId = DB::table('organizations')->where('type', 'institution')->value('id');
+            $faculties = DB::table('faculties')->get();
+
+            foreach ($faculties as $faculty) {
+                DB::table('organizations')->insert([
+                    'name' => $faculty->name,
+                    'type' => 'faculty',
+                    'parent_id' => $rootId,
+                    'is_active' => true,
+                    'metadata' => json_encode([
+                        'legacy' => [
+                            'faculty_id' => $faculty->id,
+                        ],
+                    ]),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::transaction(function () {
+            DB::table('organizations')
+                ->where('type', 'faculty')
+                ->delete();
+        });
+    }
+};
