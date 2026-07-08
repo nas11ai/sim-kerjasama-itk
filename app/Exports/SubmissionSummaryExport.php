d5f9faf6935ab1f8e6f71302b231395a77eda5dd
RaziMaulana <razifaturmaulana@gmail.com>
2026-07-08 11:52:53 +0800

    chore: fix-ci-backend

--- app/Exports/SubmissionSummaryExport.php
+++ app/Exports/SubmissionSummaryExport.php
@@ -1,13 +1,17 @@
 <?php
+
 namespace App\Exports;
 
-use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class SubmissionSummaryExport implements FromCollection, WithHeadings
 {
-    public function collection() { return collect([]); } // placeholder
+    public function collection()
+    {
+        return collect([]);
+    } // placeholder
+
     public function headings(): array
     {
         return ['ID', 'Judul', 'Status', 'Scheme', 'Lead Researcher', 'Tanggal Submit'];
