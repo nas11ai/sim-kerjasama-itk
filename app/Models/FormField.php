9404b7f567e7c7b0e718ebff17bceb85b34bb82a
Alfian Ramadani <alfianr792@gmail.com>
2026-07-23 00:17:26 +0800

    chore: refactor to using gt()

--- app/Models/FormField.php
+++ app/Models/FormField.php
@@ -67,12 +67,8 @@ class FormField extends Model
             return false;
         }
 
-        if (
-            $this->required_since !== null &&
-            $this->required_since > $submission->created_at
-        ) {
+        if ($this->required_since?->gt($submission->created_at))
             return false;
-        }
 
         return $this->is_required;
     }
